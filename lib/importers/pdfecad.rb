c9b1c8609f49e19343fd0d90dd1b06db5da4889d
vitorascorrea <vitorascorrea@gmail.com>
2016-08-14 08:42:51 -0300

    refatoracao: regex em tudo menos nomes

--- lib/importers/pdfecad.rb
+++ lib/importers/pdfecad.rb
@@ -31,18 +31,18 @@ class Importers::PdfEcad
                     :ipi => !ipi_match.empty? ? ipi_match : nil,
                     :share => line.match(/[0-9]{1,3}\,[0-9]{0,2}/).to_s.gsub(/,/,'.').to_f,
                     :society_name => line.match(/  SICAM|([0-9] [A-Z]{3,9})/).to_s[2..-1],
-                    :external_ids => [{source_name: "Ecad", source_id: line[0,10].strip}],
+                    :external_ids => [{source_name: "Ecad", source_id: line.match(/[0-9]{3,6}\s/).to_s.strip}],
                     :role => CATEGORIES[line.match(/[A-Z]{1,2}\s{1,3}[0-9]/).to_s[0,3].strip]
                    }
   end
 
   def work(line)
-    return nil if line.match(/[0-9]{1,3}\,[0-9]{0,2}/) #return nil if line is not a right holder (using share identifier)
-    aux_hash ={:iswc => line[13,15].strip,
+    return nil if line.match(/[0-9]{1,3}\,[0-9]{0,2}/) #return nil if line is not a work (using share identifier)
+    aux_hash ={:iswc => line.match(/(T\-[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9])|\-   \.   \.   \-/).to_s,
                :title => line[35,50].strip,
-               :external_ids => [{source_name: "Ecad", source_id: line[0,10].strip}],
-               :situation => line[93,8].strip,
-               :created_at => line[108,14].strip,
+               :external_ids => [{source_name: "Ecad", source_id: line.match(/[0-9]{5,10}\s/).to_s.strip}],
+               :situation => line.match(/ LB | LB\/DE | LB\/BL\/DE /).to_s.strip,
+               :created_at => line.match(/[0-9]{2}\/[0-9]{2}\/[0-9]{4}/).to_s,
                :right_holders => []
               }
   end
